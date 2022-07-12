bdc72864ff2c91713fba8989cee06a7e03f6e74b
Alperen Polat <m_alperenPolat@hotmail.com>
2022-07-12 17:35:41 +0300

    stylized changed and script updated

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 let main = document.getElementById("main");
+let sliderPointers = document.querySelector(".slider-pointers");
 
 let imageList = [
   "alexandra-gorn-JIUjvqe2ZHg-unsplash.jpg",
@@ -6,24 +7,43 @@ let imageList = [
   "vlad-sargu-ItphH2lGzuI-unsplash.jpg",
 ];
 
-let index = 0;
+let imageIndex = 0;
+let sliderIndex = -1;
 
 function changeImage() {
-  main.style.backgroundImage = `url(img/${imageList[index]})`;
-  index++;
-  if (index >= imageList.length) {
-    index = 0;
+  main.style.backgroundImage = `url(img/${imageList[imageIndex]})`;
+  imageIndex++;
+  if (imageIndex >= imageList.length) {
+    imageIndex = 0;
   }
+}
+
+function changeSliderPointer() {
+  if (sliderIndex >= 0) {
+    sliderPointers.children[sliderIndex].classList.remove("selected");
+  }
+  sliderIndex++;
+  if (sliderIndex >= imageList.length) {
+    sliderIndex = 0;
+  }
+  sliderPointers.children[sliderIndex].classList.add("selected");
 }
 
 function myCoroutine() {
   setTimeout(() => {
     console.log("Coroutine called");
     changeImage();
+    changeSliderPointer();
     myCoroutine();
   }, 5000);
 }
 
 window.addEventListener("load", () => {
   myCoroutine();
+  for (let i = 0; i < imageList.length; i++) {
+    let html = `
+        <a class="pointer"></a>
+    `;
+    sliderPointers.insertAdjacentHTML("beforeend", html);
+  }
 });
